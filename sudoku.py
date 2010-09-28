7b8bf9fe487914c1f1b4e10f228a7775f40f1abe
Richard Lapsley <lapsley.richard@gmail.com>
2010-09-28 16:06:27 +1000

    changed addingNumbers function. hangs at grid[1][8]. reason unknown

--- sudoku.py
+++ sudoku.py
@@ -1,48 +1,60 @@
+from random import randint
+
 def addingNumbers(grid):
-	#this part adds in the numbers to each row making sure that none of them
-	#are duplicating.
-	for x in range(9):
-		for y in range(9):
-			if x == 0 and y == 0:
-				grid[x][y] = randint(1,9)
-				continue
-			elif x == 0:
-                            newNumber = randint(1,9)
-                            while newNumber in grid[x]:
-                                    newNumber = randint(1,9)
-                            grid[x][y] = newNumber
-                            continue
-                        elif x != 0:
-                            holder = []
-                            for i in range(9):
-                                holder.append(grid[i][y])
-                            newNumber = randint(1,9)
-                            while (newNumber in grid[x]) and (newNumber in holder):
-                                newNumber = randint(1,9)
-                            grid[x][y] = newNumber
-                            continue
+    #this part adds in the numbers to each row making sure that none of them
+    #are duplicating.
+    for x in range(9):
+        for y in range(9):
+            print x,y
+            if x > 0:
+                holder = []
+                for i in range(9):
+                    holder.append(grid[i][x])
+                print holder
+                newNumber = randint(1,9)
+                while newNumber in grid[x] or newNumber in holder:
+                    print grid[x]
+                    print holder
+                    newNumber = randint(1,9)
+                    print newNumber
+                    raw_input(">")
+                grid[x][y] = newNumber
+            else:
+                newNumber = randint(1,9)
+                while newNumber in grid[x]:
+                    newNumber = randint(1,9)
+                grid[x][y] = newNumber
 
 def printBoard(grid):
-	#this part prints the board to the screen for visual feedback
-	hline = "    "
-	for i in range(1,10):
-		hline += "%s   " %(i)
-	hSeperator = "  +---+" + ("---+" * 8)
-	vSeperator = "  |   |" + ("   |" * 8)
-	print hline
-	print hSeperator
-	for y in range(9):
-		print vSeperator
-		print (y + 1),
-		for x in range(9):
-			print ("| %s" % (grid[x][y])),
-		print "|"
-		print vSeperator
-		print hSeperator
+    #this part prints the board to the screen for visual feedback
+    hline = "    "
+    for i in range(1,10):
+        hline += "%s   " %(i)
+    hSeperator = "  +---+" + ("---+" * 8)
+    vSeperator = "  |   |" + ("   |" * 8)
+    print hline
+    print hSeperator
+    for y in range(9):
+        print vSeperator
+        print (y + 1),
+        for x in range(9):
+            print ("| %s" % (grid[x][y])),
+        print "|"
+        print vSeperator
+        print hSeperator
 
 def makeGameBoard(grid):
-	#this part makes the grid that contains the numbers
-	for i in range(9):
-	    grid.append([])
-	    for l in range(9):
-		    grid[i].append("")
+    #this part makes the grid that contains the numbers
+    for i in range(9):
+        grid.append([])
+        for l in range(9):
+            grid[i].append("")
+
+grid = []
+raw_input("Making Game Board....")
+makeGameBoard(grid)
+raw_input("puting in numbers....")
+addingNumbers(grid)
+raw_input("printing board....")
+printBoard(grid)
+raw_input("finished")
